90c657eac7703de147fe7016bf6c72f2cab97f97
Kjell Solsvik <Kjell Solsvik>
2016-03-11 14:52:20 +0100

    Read /Write with Easygui
    Added a testground for reading and writing
    to a file using easygui.
    Added file cal_data.txt to store data.

---
+++ BirthdayCalendar/testgroundReadWrite.py
@@ -0,0 +1,35 @@
+"""
+    session 14:10
+"""
+
+import easygui
+
+
+def input_data():
+    with open("cal_data.txt", "a") as data:
+        msg = "Here you can enter some basic information"
+        title = "Get the DATA!"
+        fields = ["Fornavn", "Etternavn", "Fødselsdato"]
+        data_string = str(easygui.multenterbox(msg, title, fields))
+        data_string = data_string.replace("'", "")
+        data.write(data_string + ",\n")
+
+
+def show_data():
+    with open("cal_data.txt", "r") as data:
+        easygui.textbox("Here is the data recorded so far:", "Get the data!", data)
+    if easygui.ynbox("Do you want to enter more information?", "Get the data!"):
+        input_data()
+    else:
+        pass
+
+
+input_data()
+show_data()
+
+
+input()
+
+
+
+
